0accc719b9804f7b002728f4606c2845433328d8
antoine <antoine.gayoso@gmail.com>
2021-03-24 16:31:17 +0100

    bourbier à finir

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mysql = require('mysql')
 const router = express.Router()
 
-const pass = ""
+const pass = "#Antoine1993"
 
 function getConnection() {
   return mysql.createConnection({
