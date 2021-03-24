ffa92857aa7d133a58be11c6960d6ddb30f94009
AntoineG <76478193+rapatte@users.noreply.github.com>
2021-03-24 14:38:28 +0100

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mysql = require('mysql')
 const router = express.Router()
 
-const pass = "#Antoine1993"
+const pass = ""
 
 function getConnection() {
   return mysql.createConnection({
